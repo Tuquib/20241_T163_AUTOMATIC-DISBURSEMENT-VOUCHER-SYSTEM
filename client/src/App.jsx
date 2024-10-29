38b3541b1827f6f9f8a69bd6f34ba33066b8e340
Tuquib <2201102322@student.buksu.edu.ph>
2024-10-29 17:08:40 +0800

    front page UI

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="navbar">
+        <img src="../src/assets/Buksu_logo.png" alt="logo" className="logo" />
+        <div className="text-container">
+          <span className="logo-text">Bukidnon State University</span>
+          <span className="sub-text">Acounting Office</span>
+          <span className="sub2-text">Automatic Disbursement Voucher</span>
+        </div>
+        <nav className="nav-links">
+          <button className="login-btn">Login</button>
+        </nav>
+      </header>
+      <main className="content">
+        <img
+          src="../src/assets/background2.jpg"
+          alt="background"
+          className="background"
+        />
+      </main>
+    </div>
+  );
+}
+
+export default App;
