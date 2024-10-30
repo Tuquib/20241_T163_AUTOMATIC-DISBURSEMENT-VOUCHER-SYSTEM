be4e1fed3166f922fa40d5c62f250625c986c024
Tuquib <2201102322@student.buksu.edu.ph>
2024-10-30 21:38:57 +0800

    frontend

--- controller/adminController.js
+++
@@ -1,7 +0,0 @@
-const adminController = require("../db/adminDB");
-
-function getAll(res, res) {
-  res.status(200).json(adminController);
-}
-
-module.exports = adminController;
--- controller/staffController.js
+++
@@ -1,7 +0,0 @@
-const staffsController = require("../db/staffDB");
-
-function getAll(res, res) {
-  res.status(200).json(staffsController);
-}
-
-module.exports = staffsController;
